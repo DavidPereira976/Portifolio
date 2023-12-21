966f2bdd8521945befe939015f151f7bc2217f76
DavidPereira976 <davidpereira2002rj@gmail.com>
2023-12-21 15:14:02 -0300

    Fix: Animação em Softskills

--- src/Components/Softskills/index.jsx
+++ src/Components/Softskills/index.jsx
@@ -4,24 +4,34 @@ import Aprendizado from "../../assets/Imgs/Aprendizado.svg";
 import Equipe from "../../assets/Imgs/Equipe.svg";
 import { motion } from "framer-motion";
 
+import { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+
 import { Container, Title, ContainerImg, Moldura, Img } from "./style";
 
 function SoftSkills() {
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
   return (
     <>
       <Container>
       <motion.div
-          initial={{ opacity: 0, y: -400 }} // Estilo inicial (opcional)
-          animate={{ opacity: 1, y: 0 }} // Estilo animado
-          transition={{ duration: 0.9 }} // Configuração de transição
+          initial={{ opacity: 0, x: -2200 }} // Inicia fora da tela à esquerda
+          animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -100 }} // Anima para o centro
+          transition={{ duration: 0.9 }}
+          ref={ref}
         >
         <Title>SoftSkills</Title>
         </motion.div>
 
         <motion.div
-          initial={{ opacity: 0, y: -400 }} // Estilo inicial (opcional)
-          animate={{ opacity: 1, y: 0 }} // Estilo animado
-          transition={{ duration: 0.9 }} // Configuração de transição
+          initial={{ opacity: 0, x: -2200 }} // Inicia fora da tela à esquerda
+          animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -100 }} // Anima para o centro
+          transition={{ duration: 0.9 }}
+          ref={ref}
         >
         <ContainerImg>
           <Moldura>
